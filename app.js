e8ba5f63e7bfa327b282a1f6714f87ab340cbd27
William Donahue <baseballwilliam@gmail.com>
2024-03-07 13:24:48 -0500

    transition to Node.JS

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public')); // Serve static files from the "public" directory
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home Page' }); // Example for the home page
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Us' }); // Assuming you want to pass a title to your EJS template
+  });
+
+app.get('/contact', (req, res) => {
+res.render('contact', { title: 'Contact Us' }); // Assuming you want to pass a title to your EJS template
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
